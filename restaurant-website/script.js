33199f83d6f67b1eaabd1b1aae77be65846c29c1
tejaswini <vadlamuditejaswini299@gmail.com>
2024-12-09 23:26:40 +0530

    Add website files for Sura's Kitchen

---
+++ restaurant-website/script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-icon').addEventListener('click', function () {
+    const menuList = document.getElementById('menu-list');
+    menuList.classList.toggle('active');
+});
